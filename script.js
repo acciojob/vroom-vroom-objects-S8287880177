aa005dccc2c84bc272f0e559245dbc50b06ea7d3
SAURABH YADAV <saurabhsj069@gmail.com>
2026-01-19 23:47:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-// Complete the js code
-function Car(make, model) {}
+// Car constructor function
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Method added to Car prototype
+Car.prototype.getMakeModel = function () {
+  return this.make + " " + this.model;
+};
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+// SportsCar constructor function
+function SportsCar(make, model, topSpeed) {
+  // Call Car constructor
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
+}
+
+// Inherit from Car prototype
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// Method added to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
+
